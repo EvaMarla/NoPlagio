dee649f87fccdf5d938fecb8918e5eed2a956f5a
Eva Marla <39227995+evamarlaa@users.noreply.github.com>
2018-09-30 11:10:54 -0300

    Classe | NGrama
    Implementção de todos os requisitos de NGrama

---
+++ Corpus.py
@@ -0,0 +1,4 @@
+#Você deve criar uma classe que armazene um conjunto de documentos. Esses documentos são arquivos-texto de um diretório (documentos de referência). A classe é responsável por carregá-los e armazená-los em memória principal. Os documentos devem ser representados como acima.
+class Corpus:
+  pass
+  
---
+++ Document.py
@@ -0,0 +1,4 @@
+#Sua representação de documento deve armazenar a lista de palavras de um documento salvo em um arquivo. Essa lista de palavras, após ser carregada, deve ser armazenada em um vetor de Numpy. A classe também deverá ter uma lista encadeada (implementada por você) para guardar os n-gramas desse documento. A representação de um n-grama é dada a seguir.
+class Documents:
+  pass
+  
